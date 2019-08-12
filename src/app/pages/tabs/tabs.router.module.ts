19ff1e14be274714d9fdeea853e77e6cbc891246
elkingutierrex <elkin.gutierrex@gmail.com>
2019-08-12 09:16:48 -0500

    116 - Componentes pendientes, Estructura y Tabs

--- src/app/pages/tabs/tabs.router.module.ts
+++ src/app/pages/tabs/tabs.router.module.ts
@@ -26,17 +26,7 @@ const routes: Routes = [
               import('../tab2/tab2.module').then(m => m.Tab2PageModule)
           }
         ]
-      },
-      {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-          }
-        ]
-      },
+      },      
       {
         path: '',
         redirectTo: '/tabs/tab1',
