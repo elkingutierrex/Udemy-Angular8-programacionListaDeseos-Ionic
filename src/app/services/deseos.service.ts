58642200d22711abe56113a8c4ff3974c2870b6c
elkingutierrex <elkin.gutierrex@gmail.com>
2019-08-12 13:57:57 -0500

    122 -LocalStorage - Hacer persistente la información

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -10,16 +10,29 @@ export class DeseosService {
 
   constructor() { 
     
-    const lista1 = new Lista( 'Recolelectar piedres del infinito');
-    const lista2 = new Lista( 'Héroes a desaparecer');
+    this.cargarStorage()
+    //  const lista1 = new Lista( 'Recolelectar piedres del infinito');
+    //  const lista2 = new Lista( 'Héroes a desaparecer');
 
-    this.listas.push(lista1, lista2)
+    //  this.listas.push(lista1, lista2)
   }
 
   crearLista( titulo: string){
 
     const nuevaLista = new Lista(titulo);
     this.listas.push( nuevaLista );
+    this.guardarStorage();
 
   }
+
+  guardarStorage(){
+    localStorage.setItem('data',JSON.stringify( this.listas ))
+  }
+
+  cargarStorage(){
+    if(localStorage.getItem('data')){
+      this.listas =  JSON.parse(localStorage.getItem('data'));
+    }
+  }
+
 }
