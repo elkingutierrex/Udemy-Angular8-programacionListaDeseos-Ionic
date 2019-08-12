8ea425962d2e7b8ba70e00e0ff6fe87638c2e17f
elkingutierrex <elkin.gutierrex@gmail.com>
2019-08-12 10:29:36 -0500

    118 - Pantalla de pendientes - diseño y documentación de Ionic

--- src/app/services/deseos.service.ts
+++ src/app/services/deseos.service.ts
@@ -9,6 +9,10 @@ export class DeseosService {
   listas: Lista [] = [];
 
   constructor() { 
-    console.log("Servicio inicializado");
+    
+    const lista1 = new Lista( 'Recolelectar piedres del infinito');
+    const lista2 = new Lista( 'Héroes a desaparecer');
+
+    this.listas.push(lista1, lista2)
   }
 }
