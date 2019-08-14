7c40b503b89fe9ff7c2e67e30adecb40d2cdff73
elkingutierrex <elkin.gutierrex@gmail.com>
2019-08-14 14:54:27 -0500

    126 - Editar elementos de otras listas

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { DeseosService } from 'src/app/services/deseos.service';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { Lista } from 'src/app/models/lista.model';
 // import { type } from 'os';
 // import { Placeholder } from '@angular/compiler/src/i18n/i18n_ast';
 
@@ -54,10 +55,12 @@ export class Tab1Page {
   });
 
   alert.present();
-
-
-
  
+  }
+
+listaSeleccionada( lista : Lista){
+  console.log(lista);
+  this.router.navigateByUrl(`/tabs/tab1/agregar/${ lista.id }`) 
 }
 
 }
