b49a5b0a82d4055cc41e554a5e02a5053d7bc3df
elkingutierrex <elkin.gutierrex@gmail.com>
2020-05-21 07:36:42 -0500

    remove entryComponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  entryComponents: [],
+  // entryComponents: [],
   imports: [
     BrowserModule, 
     IonicModule.forRoot(), 
